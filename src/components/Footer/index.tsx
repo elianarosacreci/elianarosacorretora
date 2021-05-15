b46e1ec1836fc6903fc33fe48e5c2f1a3dafb190
gabrielfelipeio <gabrisilvas@outlook.com>
2021-05-15 17:30:42 -0300

    add Footer component structure and global green color shades

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,28 +1,13 @@
-import format from 'date-fns/format';
-import ptBR from 'date-fns/locale/pt-BR';
 import Link from 'next/link';
 import styles from './styles.module.scss';
 
 
 export function Header() {
 
-    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
-        locale: ptBR
-    });
-
     return (
         <header className={styles.headerContainer}>
             <Link href="/">
-                <a><h1>Eliana Rosa</h1></a>
             </Link>
-
-            <h3>Corretora de imóveis</h3>
-
-            <span>
-                <Link href="/">
-                    <a>{currentDate}</a>
-                </Link>
-            </span>
         </header>
     );
 }
