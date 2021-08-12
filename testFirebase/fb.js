32afe6dd7a0a06404672c5f99ab854c01edfa308
gabrielfelipeio <gabrisilvas@outlook.com>
2021-08-11 22:54:22 -0300

    remove testFirebase

--- testFirebase/fb.js
+++
@@ -1,9 +0,0 @@
-const firebase = require('firebase');
-
-const app = firebase.initializeApp({
-    apiKey: "AIzaSyDYFJJtruhtzMyfIzH9kYL_cEGfehnxoGs",
-    authDomain: "site-eliana-315001.firebaseapp.com",
-    databaseURL: "https://site-eliana-315001-default-rtdb.firebaseio.com",
-});
-
-module.exports = app;
--- testFirebase/test.js
+++
@@ -1,7 +0,0 @@
-const firebaseController = require('./fc')();
-
-async function run() {
-    let result = await firebaseController.getImmobileToStaticPath();
-    console.log(result);
-}
-run();
