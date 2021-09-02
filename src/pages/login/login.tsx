54f9d086a060482fb9b178632615707b30400326
gabrielfelipeio <gabrisilvas@outlook.com>
2021-09-02 19:03:34 -0300

    remove unused code

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Link from 'next/link'
 
 import styles from './login.module.scss'
-import { FaGooglePlusSquare } from 'react-icons/fa'
 
 
 export default function Login() {
@@ -17,7 +16,7 @@ export default function Login() {
                 <div className={styles.form}>
                     <form className={styles.loginForm}>
                         <Link href="/researchAdmin/researchAdmin">
-                            <button className={styles.login}>entrar com <FaGooglePlusSquare size={30} /></button>
+                            <button className={styles.login}>entrar</button>
                         </Link>
                         <a href="/" className={styles.returnToBrowse}>voltar a navegar</a>
                     </form>
