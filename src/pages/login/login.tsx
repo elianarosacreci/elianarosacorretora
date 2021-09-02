b755f9a939b08b604d661d8254ceb025e8faff9f
gabrielfelipeio <gabrisilvas@outlook.com>
2021-09-02 20:12:54 -0300

    add form on login page to set user and pass

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { FormEvent } from 'react'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 import styles from './login.module.scss'
+import { Form, Row } from 'react-bootstrap'
 
 
 export default function Login() {
+
+    const router = useRouter()
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+
+    const handleLogin = (e: FormEvent) => {
+        e.preventDefault()
+        router.push('/researchAdmin/researchAdmin')
+    }
 
     return (
         <div>
@@ -15,10 +26,18 @@ export default function Login() {
             <div className={styles.loginContainer}>
                 <div className={styles.form}>
                     <form className={styles.loginForm}>
-                        <Link href="/researchAdmin/researchAdmin">
-                            <button className={styles.login}>entrar</button>
-                        </Link>
-                        <a href="/" className={styles.returnToBrowse}>voltar a navegar</a>
+                        <Form>
+                            <Row>
+                                <Form.Group className="mb-3">
+                                    <Form.Control value={email} type="text" placeholder="E-mail" onChange={event => setEmail(event.target.value)} />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Control value={pass} type="password" placeholder="Senha" onChange={event => setPass(event.target.value)} />
+                                </Form.Group>
+                            </Row>
+                        </Form>
+                        <button className={styles.login} onClick={handleLogin}>entrar</button>
+                        <a href='/' className={styles.returnToBrowse}>voltar a navegar</a>
                     </form>
                 </div>
             </div>
