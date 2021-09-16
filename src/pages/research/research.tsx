5187a1dc465f2e30d9ddad7f6471035ac6cfc84c
gabrielfelipeio <gabrisilvas@outlook.com>
2021-09-15 22:56:26 -0300

    ajust search form to add fields and variables

--- src/pages/research/research.tsx
+++ src/pages/research/research.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 
@@ -7,6 +7,7 @@ import Image from 'next/image'
 import { Footer } from '../../components/Footer'
 
 import firebaseController from '../../services/firebaseController'
+import { Col, Form, Row } from 'react-bootstrap'
 
 
 type Immobile = {
@@ -30,6 +31,34 @@ export default function Research({ allImobiles }: ImmobileProps) {
 
     const MAX_DESCRIPTION_TITLE_LENGTH = 30;
 
+    const [immobileStatusNaPlanta, setImmobileStatusNaPlanta] = useState(false)
+    const [immobileStatusEmConstrucao, setImmobileStatusEmConstrucao] = useState(false)
+    const [immobileStatusProntoPraMorar, setImmobileStatusProntoPraMorar] = useState(false)
+
+    const [immobileKindApartamento, setImmobileKindApartamento] = useState(false)
+    const [immobileKindCobertura, setImmobileKindCobertura] = useState(false)
+    const [immobileKindCasa, setImmobileKindCasa] = useState(false)
+    const [immobileKindCasaCondominio, setImmobileKindCasaCondominio] = useState(false)
+    const [immobileKindTerreno, setImmobileKindTerreno] = useState(false)
+    const [immobileKindConjuntoComercial, setImmobileKindConjuntoComercial] = useState(false)
+    const [immobileKindGalpao, setImmobileKindGalpao] = useState(false)
+    const [immobileKindSitioFazenda, setImmobileKindSitioFazenda] = useState(false)
+    const [immobileKindPredioInteiro, setImmobileKindPredioInteiro] = useState(false)
+    const [immobileKindLoja, setImmobileKindLoja] = useState(false)
+    const [immobileKindImovelComercial, setImmobileKindImovelComercial] = useState(false)
+
+    const [immobilePriceMin, setImmobilePriceMin] = useState('')
+    const [immobilePriceMax, setImmobilePriceMax] = useState('')
+    const [immobileFootageMin, setImmobileFootageMin] = useState('')
+    const [immobileFootageMax, setImmobileFootageMax] = useState('')
+    const [immobileBedrooms, setImmobileBedrooms] = useState('')
+    const [immobileBathrooms, setImmobileBathrooms] = useState('')
+    const [immobileVacancies, setImmobileVacancies] = useState('')
+    const [immobileState, setImmobileState] = useState('')
+    const [immobileCity, setImmobileCity] = useState('')
+    const [immobileFeatures, setImmobileFeatures] = useState('')
+
+
     return (
         <div>
             <Head>
@@ -40,106 +69,128 @@ export default function Research({ allImobiles }: ImmobileProps) {
 
                 <div className={styles.researchContent}>
 
-                    <div className={styles.research}>
-                        <button className={styles.researchButton} type="button">Buscar</button>
-                    </div>
-
-                    <div className={styles.address}>
-                        <div className={styles.addressLocation}>
-                            <label>Localização do Imóvel</label>
-                            <input
-                                type="text"
-                                placeholder="Digite"
-                            />
-                        </div>
-                    </div>
-
-                    <div className={styles.price}>
-                        <div className={styles.priceMin}>
-                            <label>Preço Mínimo</label>
-                            <input
-                                type="text"
-                                placeholder="R$"
-                            />
-                        </div>
-                        <div className={styles.priceMax}>
-                            <label>Preço Máximo</label>
-                            <input
-                                type="text"
-                                placeholder="R$"
-                            />
-                        </div>
-                    </div>
-
-                    <div className={styles.footage}>
-                        <div className={styles.footageMin}>
-                            <label>Área Mínima</label>
-                            <input
-                                type="text"
-                                placeholder="m²"
-                            />
-                        </div>
-                        <div className={styles.footageMax}>
-                            <label>Área Máxima</label>
-                            <input
-                                type="text"
-                                placeholder="m²"
-                            />
-                        </div>
-                    </div>
-
-                    <div className={styles.bedrooms}>
-                        <div className={styles.bedroomsNumbers}>
-                            <label>Número de Quartos</label>
-                            <div className={styles.buttons}>
-                                <button>1+</button>
-                                <button>2+</button>
-                                <button>3+</button>
-                                <button>4+</button>
-                                <button>5+</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={styles.bathrooms}>
-                        <div className={styles.bathroomsNumbers}>
-                            <label>Número de Banheiros</label>
-                            <div className={styles.buttons}>
-                                <button>1+</button>
-                                <button>2+</button>
-                                <button>3+</button>
-                                <button>4+</button>
-                                <button>5+</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={styles.vacancies}>
-                        <div className={styles.vacanciesNumbers}>
-                            <label>Número de Vagas de Garagem</label>
-                            <div className={styles.buttons}>
-                                <button>1+</button>
-                                <button>2+</button>
-                                <button>3+</button>
-                                <button>4+</button>
-                                <button>5+</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={styles.status}>
-                        <div className={styles.statusImmobiles}>
-                            <label>Status do Imóvel</label>
-                            <div className={styles.statusOptions}>
-                                <input type="checkbox" id="naPlanta" name="naPlanta" value="naPlanta" />
-                                <label htmlFor="naPlanta"> na planta</label><br />
-                                <input type="checkbox" id="emConstrucao" name="emConstrucao" value="emConstrucao" />
-                                <label htmlFor="emConstrucao"> em construção</label><br />
-                                <input type="checkbox" id="prontoParaMorar" name="prontoParaMorar" value="prontoParaMorar" />
-                                <label htmlFor="prontoParaMorar"> pronto pra morar</label>
-                            </div>
-                        </div>
-                    </div>
+                    <Form>
+                        <Row>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Preço Mínimo</Form.Label>
+                                    <Form.Control value={immobilePriceMin.toString()} type="text" onChange={event => setImmobilePriceMin(event.target.value)} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Preço Máximo</Form.Label>
+                                    <Form.Control value={immobilePriceMax.toString()} type="text" onChange={event => setImmobilePriceMax(event.target.value)} />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Área Mínima</Form.Label>
+                                    <Form.Control value={immobileFootageMin} type="number" onChange={event => setImmobileFootageMin(event.target.value)} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Área Máxima</Form.Label>
+                                    <Form.Control value={immobileFootageMax} type="number" onChange={event => setImmobileFootageMax(event.target.value)} />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Quartos</Form.Label>
+                                    <Form.Control value={immobileBedrooms} type="number" onChange={event => setImmobileBedrooms(event.target.value)} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Banheiros</Form.Label>
+                                    <Form.Control value={immobileBathrooms} type="number" onChange={event => setImmobileBathrooms(event.target.value)} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Vagas</Form.Label>
+                                    <Form.Control value={immobileVacancies} type="number" onChange={event => setImmobileVacancies(event.target.value)} />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={4}>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Estado</Form.Label>
+                                    <Form.Control value={immobileState} type="text" onChange={event => setImmobileState(event.target.value)} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Cidade</Form.Label>
+                                    <Form.Control value={immobileCity} type="text" onChange={event => setImmobileCity(event.target.value)} />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Lazer & Características</Form.Label>
+                                <Form.Control as="textarea" value={immobileFeatures} type="text" onChange={event => setImmobileFeatures(event.target.value)} />
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Label className="mb-3">Status do Imóvel</Form.Label>
+                            <Form.Group className="mb-3">
+                                <Form.Check inline name="tipo" type="checkbox" label="Apartamento" onChange={() => {
+                                    immobileKindApartamento ? setImmobileKindApartamento(false) : setImmobileKindApartamento(true)
+                                }} checked={immobileKindApartamento} />
+                                <Form.Check inline name="tipo" type="checkbox" label="Cobertura" onChange={() => {
+                                    immobileKindCobertura ? setImmobileKindCobertura(false) : setImmobileKindCobertura(true)
+                                }} checked={immobileKindCobertura} />
+                                <Form.Check inline name="tipo" type="checkbox" label="Casa" onChange={() => {
+                                    immobileKindCasa ? setImmobileKindCasa(false) : setImmobileKindCasa(true)
+                                }} checked={immobileKindCasa} />
+                                <Form.Check inline name="tipo" type="checkbox" label="Casa de Condominio" onChange={() => {
+                                    immobileKindCasaCondominio ? setImmobileKindCasaCondominio(false) : setImmobileKindCasaCondominio(true)
+                                }} checked={immobileKindCasaCondominio} />
+                                <Form.Check inline name="tipo" type="checkbox" label="Terreno" onChange={() => {
+                                    immobileKindTerreno ? setImmobileKindTerreno(false) : setImmobileKindTerreno(true)
+                                }} checked={immobileKindTerreno} />
+                                <Form.Check inline name="tipo" type="checkbox" label="Conjunto Comercial" onChange={() => {
+                                    immobileKindConjuntoComercial ? setImmobileKindConjuntoComercial(false) : setImmobileKindConjuntoComercial(true)
+                                }} checked={immobileKindConjuntoComercial} />
+                                <Form.Check inline name="tipo" type="checkbox" label="Galpão" onChange={() => {
+                                    immobileKindGalpao ? setImmobileKindGalpao(false) : setImmobileKindGalpao(true)
+                                }} checked={immobileKindGalpao} />
+                                <Form.Check inline name="tipo" type="checkbox" label="Sitio/Fazenda" onChange={() => {
+                                    immobileKindSitioFazenda ? setImmobileKindSitioFazenda(false) : setImmobileKindSitioFazenda(true)
+                                }} checked={immobileKindSitioFazenda} />
+                                <Form.Check inline name="tipo" type="checkbox" label="Prédio Inteiro" onChange={() => {
+                                    immobileKindPredioInteiro ? setImmobileKindPredioInteiro(false) : setImmobileKindPredioInteiro(true)
+                                }} checked={immobileKindPredioInteiro} />
+                                <Form.Check inline name="tipo" type="checkbox" label="Loja" onChange={() => {
+                                    immobileKindLoja ? setImmobileKindLoja(false) : setImmobileKindLoja(true)
+                                }} checked={immobileKindLoja} />
+                                <Form.Check inline name="tipo" type="checkbox" label="Imóvel Comercial" onChange={() => {
+                                    immobileKindImovelComercial ? setImmobileKindImovelComercial(false) : setImmobileKindImovelComercial(true)
+                                }} checked={immobileKindImovelComercial} />
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Label className="mb-3">Status do Imóvel</Form.Label>
+                            <Form.Group className="mb-3">
+                                <Form.Check inline name="status" type="checkbox" label="Na Planta" onChange={() => {
+                                    immobileStatusNaPlanta ? setImmobileStatusNaPlanta(false) : setImmobileStatusNaPlanta(true)
+                                }} checked={immobileStatusNaPlanta} />
+                                <Form.Check inline name="status" type="checkbox" label="Em Construção" onChange={() => {
+                                    immobileStatusEmConstrucao ? setImmobileStatusEmConstrucao(false) : setImmobileStatusEmConstrucao(true)
+                                }} checked={immobileStatusEmConstrucao} />
+                                <Form.Check inline name="status" type="checkbox" label="Pronto pra Morar" onChange={() => {
+                                    immobileStatusProntoPraMorar ? setImmobileStatusProntoPraMorar(false) : setImmobileStatusProntoPraMorar(true)
+                                }} checked={immobileStatusProntoPraMorar} />
+                            </Form.Group>
+                        </Row>
+                    </Form>
 
                 </div>
 
